51a1f10679377e202ef4b4e03be9ae68ff98cf08
Bensolve <54053468+Benjamin569@users.noreply.github.com>
2023-12-14 14:59:25 +0000

    hello

--- pages/GhanaTour.tsx
+++ pages/GhanaTour.tsx
@@ -1,63 +1,63 @@
-// MyComponent.js
-import React from 'react';
-import '@/components/mobileSlide.jsx';
-import { ChevronRight, ChevronLeft } from 'lucide-react';
-import '.@/components/scripts.js';
-import Slide from '@/components/Slide';
+// // MyComponent.js
+// import React from 'react';
+// import '@/components/mobileSlide.jsx';
+// import { ChevronRight, ChevronLeft } from 'lucide-react';
+// import '.@/components/scripts.js';
+// import Slide from '@/components/Slide';
 
-const Ghanatour = () => {
-  const slidesData = [
-    {
-      imageUrl: 'ghana1.jpg',
-      name: 'Kakum National Park - Ghana',
-      description: '',
-    },
-    {
-      imageUrl: 'ghana2.jpg',
-      name: 'Mole National Park - Ghana',
-      description: '',
-    },
-    {
-      imageUrl: '3.jpg',
-      name: 'LUNDEV',
-      description: '',
-    },
-    {
-      imageUrl: '4.jpg',
-      name: 'LUNDEV',
-      description: '',
-    },
-    {
-      imageUrl: '5.jpg',
-      name: 'LUNDEV',
-      description: '',
-    },
-    {
-      imageUrl: 'back3.jpg',
-      name: 'Hello',
-      description: '',
-    },
-  ];
+// const Ghanatour = () => {
+//   const slidesData = [
+//     {
+//       imageUrl: 'ghana1.jpg',
+//       name: 'Kakum National Park - Ghana',
+//       description: '',
+//     },
+//     {
+//       imageUrl: 'ghana2.jpg',
+//       name: 'Mole National Park - Ghana',
+//       description: '',
+//     },
+//     {
+//       imageUrl: '3.jpg',
+//       name: 'LUNDEV',
+//       description: '',
+//     },
+//     {
+//       imageUrl: '4.jpg',
+//       name: 'LUNDEV',
+//       description: '',
+//     },
+//     {
+//       imageUrl: '5.jpg',
+//       name: 'LUNDEV',
+//       description: '',
+//     },
+//     {
+//       imageUrl: 'back3.jpg',
+//       name: 'Hello',
+//       description: '',
+//     },
+//   ];
 
-  return (
-    <section className=" ">
-      <div className="container ">
-        <div id="slide" className=''>
-          {slidesData.map((slide, index) => (
-            <Slide key={index} {...slide} />
-          ))}
-        </div>
-        <div className="buttons">
-          <button id="prev" className="text-white icon-button">
-            <ChevronLeft className="" />
-          </button>
-          <button id="next" className="text-white icon-button">
-            <ChevronRight />
-          </button>
-        </div>
-      </div>
-    </section>
-  );
-};
+//   return (
+//     <section className=" ">
+//       <div className="container ">
+//         <div id="slide" className=''>
+//           {slidesData.map((slide, index) => (
+//             <Slide key={index} {...slide} />
+//           ))}
+//         </div>
+//         <div className="buttons">
+//           <button id="prev" className="text-white icon-button">
+//             <ChevronLeft className="" />
+//           </button>
+//           <button id="next" className="text-white icon-button">
+//             <ChevronRight />
+//           </button>
+//         </div>
+//       </div>
+//     </section>
+//   );
+// };
 
-export default Ghanatour;
+// export default Ghanatour;
