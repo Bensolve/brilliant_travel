19103b0259ba71628484029f23336878a3a975bd
Bensolve <54053468+Benjamin569@users.noreply.github.com>
2024-01-27 20:16:36 +0000

    verifcation for google

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,6 +22,7 @@ export default function RootLayout({
     <html lang="en">
        <head>
         <meta charSet="utf-8" />
+        <meta name="google-site-verification" content="A5zxzFlJPm_GjdXlVEfZkZtFXXW9RE4lX7dmQXUU7w8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
       <body>
